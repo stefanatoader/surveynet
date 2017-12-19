03300f0848a02e37e7dcc41227be1272883ab14d
Unknown <minutivlad@gmail.com>
2017-12-19 03:01:38 +0200

    added entities

---
+++ Entities/Form.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities
+{
+    class Form
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public List<Account> Autors { get; set; }
+
+        public List<Guid> Order { get; set; }
+    }
+}
---
+++ Entities/Question.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities
+{
+    class Question
+    {
+        public Guid Id { get; set; }
+        public string QuestionText { get; set; }
+    }
+}
